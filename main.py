83b78574b57264c43f130ef3ddd3d1efe9f28f2c
Przemyslaw Sobol <przemyslaw.sobol@gmail.com>
2022-12-18 15:07:59 +0100

    Dodanie pliku main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from pytube import YouTube
+
+while True:
+    try:
+        user_link = input("Wklej link z Youtuba:")
+        yt = YouTube(user_link)
+        print("Title: ", yt.title)
+        print("Lenght:", yt.length, "sec")
+        mp3 = yt.streams.get_audio_only()
+        downloaded_mp3 = mp3.download(output_path="./PobraneMP3/")
+        base, ext = os.path.splitext(downloaded_mp3)
+        new_file = base + ".mp3"
+        os.rename(downloaded_mp3, new_file)
+    except:
+        print("Podałeś nieprawidłowy link")
+
+        
+
+
